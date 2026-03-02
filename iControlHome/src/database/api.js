6c2834ef894c660490cd243acf41a0a5f58c0464
Dao Hoang Anh <hoanganhdao2k3@gmail.com>
2026-03-02 22:53:57 +0700

    lil sh1

--- iControlHome/src/database/api.js
+++ iControlHome/src/database/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.56.1:3000/api'; 
+const BASE_URL = 'http://192.168.1.110:3000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
