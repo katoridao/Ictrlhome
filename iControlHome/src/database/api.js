dd443dd322687ee2847c5e5a2a2fa0b955635b70
Admin <hoangtiep01623@gmail.com>
2026-03-10 18:18:59 +0700

    Merge branch 'main' into Tiep

--- iControlHome/src/database/api.js
+++ iControlHome/src/database/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://192.168.100.91:3000/api';
+const BASE_URL = 'http://192.168.1.61:3000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
